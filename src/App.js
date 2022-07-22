dd24f2c43e8c72ecc3b25b717d31f197f0fa3b43
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-07-23 09:12:25 +1200

    tested the context api
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Navbar, Search, Info } from './Components'
+import { Navbar, Search, Info } from './components'
 
 const App = () => {
+
 	return (
 		<>
-			<Navbar />
+			<Navbar/>
 			<Search />
 			<Info />
 		</>
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {useGlobalContext} from '../context/context'
 
 const Info = () => {
+	const data = useGlobalContext();  /*test*/
 	return (
 		<>
-			<h1>Info Component goes here</h1>
+			<h1>Info Component goes here : {data}</h1>
 		</>
 	)
 }
